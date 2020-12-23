1f7a9e1b889b46020052044d573cf85ea6c88610
Lucas Levino <lucao107@hottmail.com>
2020-12-22 21:40:11 -0400

    refactor: refactor in styles

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,7 +4,11 @@ import Styles from './login-styles.scss'
 const Login: React.FC = () => {
   return (
     <div className={ Styles.login }>
-        Hello
+      <header className={ Styles.header }></header>
+
+      <form className={ Styles.form }></form>
+
+      <footer className={ Styles.footer }></footer>
     </div>
   )
 }
